855432a4e48d70abc657a8e3e63008ce61bb10de
GosTraight2020 <shanlin.liu@intel.com>
2022-03-15 12:56:15 +0800

    Update run.sh

--- unit_test/about_ut/script/run.sh
+++ unit_test/about_ut/script/run.sh
@@ -11,7 +11,7 @@ part_id=$(echo $commit_id | cut -c 1-7)
 log_title=$currentTime-$part_id
 
 cd /DeepRec \
-&& bash -x /about_ut/script/ut.sh | tee /about_ut/log/$currentTime/ut_res.log\
+&& bash -x /about_ut/script/ut.sh | tee /about_ut/log/$log_title/ut_res.log\
 && bazel clean\
 && pip install -r /about_ut/script/requirements.txt \
 && cd /about_ut/script/ \
