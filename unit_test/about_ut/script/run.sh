015db176a386a5491f74678e2f11cad5514f460b
GosTraight2020 <shanlin.liu@intel.com>
2022-03-15 09:57:19 +0800

    Update run.sh

--- unit_test/about_ut/script/run.sh
+++ unit_test/about_ut/script/run.sh
@@ -6,11 +6,15 @@ proxy_address=http://child-prc.intel.com:913
 export http_proxy=$proxy_address
 export https_proxy=$proxy_address
 
+commit_id=$(cat ../../config.properites|grep commit| awk -F " " '{print $2}')
+part_id=$(echo $commit_id| cut -c 1-7)
+part_date=$(echo $currentTime | cut -c 3-8)
+log_title=$part_date-$part_id
 
 cd /DeepRec \
 && bash -x /about_ut/script/ut.sh | tee /about_ut/log/$currentTime/ut_res.log\
 && bazel clean\
 && pip install -r /about_ut/script/requirements.txt \
 && cd /about_ut/script/ \
-&& echo ------------------ >> /about_ut/log/$currentTime/ut_res.log\
-&& python /about_ut/script/ExeclWriter.py BM /about_ut/log/$currentTime/ut_res.log
+&& echo ------------------ >> /about_ut/log/$log_title/ut_res.log\
+&& python /about_ut/script/ExeclWriter.py BM /about_ut/log/$log_title/ut_res.log
