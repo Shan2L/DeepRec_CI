f25faca3799a2ecf13f739e989d08c6a238b065f
GosTraight2020 <shanlin.liu@intel.com>
2022-03-18 16:55:05 +0800

    Update run.sh

--- unit_test/about_ut/script/run.sh
+++ unit_test/about_ut/script/run.sh
@@ -2,6 +2,7 @@
 
 currentTime=$1
 commit_id=$2
+mkl_tag=$3
 
 proxy_address=http://child-prc.intel.com:913
 export http_proxy=$proxy_address
@@ -9,6 +10,7 @@ export https_proxy=$proxy_address
 
 part_id=$(echo $commit_id | cut -c 1-7)
 log_title=$currentTime-$part_id
+log_title=${log_title}_with_${mkl_tag}
 
 cd /DeepRec \
 && bash -x /about_ut/script/ut.sh | tee /about_ut/log/$log_title/ut_res.log\
