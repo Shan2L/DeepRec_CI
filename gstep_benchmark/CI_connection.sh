da2c85a2c17a4c5b30183826ab75929d37e8f642
GosTraight2020 <shanlin.liu@intel.com>
2022-03-22 22:36:18 +0800

    Update CI_connection.sh

--- gstep_benchmark/CI_connection.sh
+++ gstep_benchmark/CI_connection.sh
@@ -64,19 +64,19 @@ function make_cmd()
 	for model in $model_list
 	do
 			[[ $deeprec_param1 != "/" ]]&&sed -i "/##########1/a\CMD ${model}_deeprec_bf16:python train.py | $deeprec_param1" $config_file
-			[[ $deeprec_param2 != "/" ]]&&sed -i "/##########1/a\CMD ${model}_deeprec_bf16:python train.py | $deeprec_param2" $config_file	
+			[[ $deeprec_param2 != "/" ]]&& [[ $weekly_tag != 'true' ]] && sed -i "/##########1/a\CMD ${model}_deeprec_bf16:python train.py | $deeprec_param2" $config_file	
 	done
 		
 	for model in $model_list
 	do
 			[[ $deeprec_param1 != "/" ]]&&sed -i "/##########3/a\CMD ${model}_deeprec_fp32:python train.py | $deeprec_param1" $config_file
-			[[ $deeprec_param2 != "/" ]]&&sed -i "/##########3/a\CMD ${model}_deeprec_fp32:python train.py | $deeprec_param2" $config_file
+			[[ $deeprec_param2 != "/" ]]&& [[ $weekly_tag != 'true' ]] && sed -i "/##########3/a\CMD ${model}_deeprec_fp32:python train.py | $deeprec_param2" $config_file
 	done
 	
 	for model in $model_list
 	do
 			[[ $tf_param1 != '/' ]] && sed -i "/##########5/a\CMD ${model}_tf_fp32:python train.py | $tf_param1" $config_file
-			[[ $tf_param2 != '/' ]] && sed -i "/##########5/a\CMD ${model}_tf_fp32:python train.py | $tf_param2" $config_file
+			[[ $tf_param2 != '/' ]] && [[ $weekly_tag != 'true' ]] && sed -i "/##########5/a\CMD ${model}_tf_fp32:python train.py | $tf_param2" $config_file
 	done
 	echo "finished"
 
@@ -87,6 +87,7 @@ deeprec_param1=$2
 deeprec_param2=$3
 tf_param1=$4
 tf_param2=$5
+weekly_tag=$6
 
 
 config_file="./config.properties"
