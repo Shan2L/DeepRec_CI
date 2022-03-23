477c7343179d5c05c37e070ce45ffee21eec79b0
GosTraight2020 <shanlin.liu@intel.com>
2022-03-23 11:03:41 +0800

    Update CI_connection.sh

--- gstep_benchmark/CI_connection.sh
+++ gstep_benchmark/CI_connection.sh
@@ -63,31 +63,31 @@ function make_cmd()
 
 	for model in $model_list
 	do
-			[[ $deeprec_param1 != "/" ]]&&sed -i "/##########1/a\CMD ${model}_deeprec_bf16:python train.py | $deeprec_param1" $config_file
-			[[ $deeprec_param2 != "/" ]]&& [[ $weekly_tag != 'true' ]] && sed -i "/##########1/a\CMD ${model}_deeprec_bf16:python train.py | $deeprec_param2" $config_file	
+			[[ $deeprec_bf16_param1 != "/" ]]&&sed -i "/##########1/a\CMD ${model}_deeprec_bf16:python train.py | $deeprec_param1" $config_file
+			[[ $deeprec_bf16_param2 != "/" ]]&&sed -i "/##########1/a\CMD ${model}_deeprec_bf16:python train.py | $deeprec_param2" $config_file	
 	done
 		
 	for model in $model_list
 	do
-			[[ $deeprec_param1 != "/" ]]&&sed -i "/##########3/a\CMD ${model}_deeprec_fp32:python train.py | $deeprec_param1" $config_file
-			[[ $deeprec_param2 != "/" ]]&& [[ $weekly_tag != 'true' ]] && sed -i "/##########3/a\CMD ${model}_deeprec_fp32:python train.py | $deeprec_param2" $config_file
+			[[ $deeprec_fp32_param1 != "/" ]]&&sed -i "/##########3/a\CMD ${model}_deeprec_fp32:python train.py | $deeprec_param1" $config_file
+			[[ $deeprec_fp32_param2 != "/" ]]&&sed -i "/##########3/a\CMD ${model}_deeprec_fp32:python train.py | $deeprec_param2" $config_file
 	done
 	
 	for model in $model_list
 	do
-			[[ $tf_param1 != '/' ]] && sed -i "/##########5/a\CMD ${model}_tf_fp32:python train.py | $tf_param1" $config_file
-			[[ $tf_param2 != '/' ]] && [[ $weekly_tag != 'true' ]] && sed -i "/##########5/a\CMD ${model}_tf_fp32:python train.py | $tf_param2" $config_file
+			[[ $tf_param != '/' ]] && sed -i "/##########5/a\CMD ${model}_tf_fp32:python train.py | $tf_param1" $config_file
 	done
 	echo "finished"
 
 }
 
 model_list=$1
-deeprec_param1=$2
-deeprec_param2=$3
-tf_param1=$4
-tf_param2=$5
-weekly_tag=$6
+deeprec_bf16_param1=$2
+deeprec_bf16_param2=$3
+deeprec_fp32_param1=$4
+deeprec_fp32_param=$5
+tf_param=$6
+weekly_tag=$7
 
 
 config_file="./config.properties"
