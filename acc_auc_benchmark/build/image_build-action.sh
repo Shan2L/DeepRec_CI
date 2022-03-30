6f17ffde5bc509d768e75b8e6e3840f3a35a19b0
GosTraight2020 <shanlin.liu@intel.com>
2022-03-30 22:20:27 +0800

    Update image_build-action.sh

--- acc_auc_benchmark/build/image_build-action.sh
+++ acc_auc_benchmark/build/image_build-action.sh
@@ -45,8 +45,7 @@ function runDockerContrainer(){
     
     if [[ $container_name == "whl_build" ]];then
 	sudo docker volume create ut_cache
-    	optional="-v  $cache_path:/root/.cache\ 
-		 --rm"
+    	optional="-v  $cache_path:/root/.cache --rm"
     fi
     
     sudo docker run \
