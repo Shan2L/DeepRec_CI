2b7a40b59cf4d4de9667c7b0704a768fac58d697
GosTraight2020 <shanlin.liu@intel.com>
2022-04-08 11:16:46 +0800

    Update check_model.sh

--- gstep_benchmark/benchmark_result/record/tool/check_model.sh
+++ gstep_benchmark/benchmark_result/record/tool/check_model.sh
@@ -1,9 +1,11 @@
 # modify the shell command line to comments, by given column id or regexp
 function modify_text()
-{       
-        col_id=$(cat $src_file | grep -n $regex | awk -F ':' '{print$1}')
-        content=$(cat $src_file | grep -n $regex | awk -F ':' '{print$2}')
-        sed -i '${col_id}c # $content' $scr_file
+{
+        regex1=$1
+        regex2=$2
+        col_id=$(cat $src_file | grep -n $regex1 | grep $regex2  |awk -F ':' '{print $1}')
+        content=$(cat $src_file | grep -n $regex1 | grep $regex2 | awk -F ':' '{print $2}')
+        sed -i "${col_id}c # $content" $src_file
 }
 
 
@@ -12,15 +14,18 @@ currentTime=$2
 test_list=$3
 
 exist_list=$(ls -F /root/modelzoo | grep '/'|sed 's/\///g')
-src_file=/bechmark_result/record/script/$currentTime/${catg}.sh
+echo $exist_list
+echo $test_list
+src_file=/benchmark_result/record/script/$currentTime/${catg}.sh
 
 for model in $test_list
 do
         if [[ -z $(echo $exist_list | grep $model) ]];then
                 echo "[ERROR] Modle $model does not exists"
-                modify_text regex 'testing $model'
-                modify_text regex 'cd /root/modelzoo/'
-                modify_text regex '${model,,}_$catg'
+                modify_text  "echo" $model
+                modify_text  "cd" "/root/modelzoo/$model"
+                modify_text  "timeline" "${model,,}"
         fi
 done
+
 
